c03dc27505271295c6d688e1d22027a66df37676
denisp22 <denispetrov722@gmail.com>
2020-01-08 11:48:01 -0600

    login control input

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,38 +1,71 @@
 import React from 'react'
 import { Button, Divider, Form, Grid, Segment, Container } from 'semantic-ui-react'
+import { withRouter } from 'react-router-dom'
 
-const LoginForm = () => (
-// Container gives box some room on sides and top
-  <Container style={{width: 500, marginTop: '6em'}}>
-  <Segment placeholder>
-    <Grid style={{height: 330}} columns={2} relaxed='very' stackable>
-      <Grid.Column verticalAlign='middle'>
-        <Form>
-          <Form.Input
-            icon='user'
-            iconPosition='left'
-            label='Username'
-            placeholder='Username'
-          />
-          <Form.Input
-            icon='lock'
-            iconPosition='left'
-            label='Password'
-            type='password'
-          />
+class LoginForm extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+    
+    routeToSignup = () => {
+        this.props.history.push('/signup')
+    }
 
-          <Button content='Login' primary />
-        </Form>
-      </Grid.Column>
+    handleInputChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
 
-      <Grid.Column verticalAlign='middle'>
-        <Button content='Sign up' icon='signup' size='big' />
-      </Grid.Column>
-    </Grid>
+    handleSubmit = event => {
+        event.preventDefault()
+        console.log(event.target)
+    }
+    
+    render() {
+        return(
+            // Container gives box some room on sides and top
+            <Container style={{width: 500, marginTop: '6em'}}>
+            <Segment placeholder>
+                <Grid style={{height: 330}} columns={2} relaxed='very' stackable>
+                <Grid.Column verticalAlign='middle'>
+                    <Form onSubmit={this.handleSubmit}>
+                    <Form.Input
+                        onChange={this.handleInputChange}
+                        icon='user'
+                        iconPosition='left'
+                        placeholder='Username'
+                        name="username"
+                        value={this.state.username}
+                    />
+                    <Form.Input
+                        onChange={this.handleInputChange}
+                        icon='lock'
+                        iconPosition='left'
+                        type='password'
+                        placeholder='Password'
+                        name="password"
+                        value={this.state.password}
+                    />
 
-    <Divider vertical>Or</Divider>
-  </Segment>
-  </Container>
-)
+                    <Button style={{marginTop: '3em'}} content='Login' primary />
+                    </Form>
+                </Grid.Column>
 
-export default LoginForm
+                <Grid.Column verticalAlign='middle'>
+                    <Button onClick={this.routeToSignup} content='Sign up' icon='signup' size='big' />
+                </Grid.Column>
+                </Grid>
+
+                <Divider vertical>Or</Divider>
+            </Segment>
+            </Container>
+        )
+    }
+}
+
+export default withRouter(LoginForm)
--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,54 +1,62 @@
 import React from 'react'
 import { Button, Divider, Form, Grid, Segment, Container, Header } from 'semantic-ui-react'
+import { withRouter } from 'react-router-dom'
 
-const SignupForm = () => (
-// Container gives box some room on sides and top
-  <Container style={{width: 600, marginTop: '6em'}}>
-    <Segment placeholder>
-        <Grid style={{height: 500}} relaxed='very' stackable>
-        <Grid.Row >
-            <Grid.Column>
-            <Form>
-            <Form.Group style={{marginTop: '3em'}} widths='equal'>
-            <Form.Input
-                icon='id card'
-                iconPosition='left'
-                placeholder='Full Name'
-            />
-            <Form.Input
-                icon='mail'
-                iconPosition='left'
-                type='password'
-                placeholder='Email'
-            />
-            </Form.Group>
-            <Form.Group style={{marginTop: '3em'}} widths='equal'>
-            <Form.Input
-                icon='user'
-                iconPosition='left'
-                placeholder='Username'
-            />
-            <Form.Input
-                icon='lock'
-                iconPosition='left'
-                type='password'
-                placeholder='Password'
-            />
-            </Form.Group>
-            <Button style={{marginTop: '4em'}} content='Create Account' primary />
-            </Form>
-            </Grid.Column>
-        </Grid.Row>
-        <Divider ></Divider>
-        <Grid.Row >
-            <Grid.Column>
-            <Header textAlign="center" as='h3'>Already have an account?</Header>
-            <Button content='Login' size='big' />
-            </Grid.Column>
-        </Grid.Row>
-        </Grid>
-    </Segment>
-  </Container>
-)
+const SignupForm = (props) => {
+    const routeToLogin = () => {
+        props.history.push('/login')
+    }
 
-export default SignupForm
+    return (
+        // Container gives box some room on sides and top
+        <Container style={{width: 600, marginTop: '6em'}}>
+            <Segment placeholder>
+                <Grid style={{height: 500}} relaxed='very' stackable>
+                <Grid.Row >
+                    <Grid.Column>
+                    <Form>
+                    <Form.Group style={{marginTop: '3em'}} widths='equal'>
+                    <Form.Input
+                        icon='id card'
+                        iconPosition='left'
+                        placeholder='Full Name'
+                    />
+                    <Form.Input
+                        icon='mail'
+                        iconPosition='left'
+                        type='password'
+                        placeholder='Email'
+                    />
+                    </Form.Group>
+                    <Form.Group style={{marginTop: '3em'}} widths='equal'>
+                    <Form.Input
+                        icon='user'
+                        iconPosition='left'
+                        placeholder='Username'
+                    />
+                    <Form.Input
+                        icon='lock'
+                        iconPosition='left'
+                        type='password'
+                        placeholder='Password'
+                    />
+                    </Form.Group>
+                    <Button style={{marginTop: '4em'}} content='Create Account' primary />
+                    </Form>
+                    </Grid.Column>
+                </Grid.Row>
+                <Divider ></Divider>
+                <Grid.Row >
+                    <Grid.Column>
+                    <Header textAlign="center" as='h3'>Already have an account?</Header>
+                    <Button onClick={routeToLogin} content='Login' size='big' />
+                    </Grid.Column>
+                </Grid.Row>
+                </Grid>
+            </Segment>
+        </Container>
+    )
+
+}
+
+export default withRouter(SignupForm)
