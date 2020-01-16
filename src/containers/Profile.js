3afb57a3793fc11e06d2e315e87f191090901b91
denisp22 <denispetrov722@gmail.com>
2020-01-15 18:44:05 -0600

    follow unfollow on profile page

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -4,6 +4,7 @@ import { Grid, Card, Image, Button } from 'semantic-ui-react'
 import ReviewFeedCard from '../components/ReviewFeedCard'
 import WithAuth from '../components/WithAuth'
 import { connect } from 'react-redux'
+import { addLeader, removeLeader } from '../actions/index'
 
 class Profile extends React.Component {
     constructor() {
@@ -30,6 +31,52 @@ class Profile extends React.Component {
         this.props.history.push(`/profile/${this.state.user.id}/edit`)
     }
 
+    renderEditButton = () => {
+        return <Button onClick={this.handleEditProfile} content='Edit Profile' />
+    }
+
+    followFetch = (postOrDelete) => {
+        const fetchObj = {
+            method: `${postOrDelete}`,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ leader_id: this.state.user.id, follower_id: this.props.user.id})
+        }
+        
+        if (postOrDelete === 'POST') {
+            fetch('http://localhost:3000/follows', fetchObj)
+            .then(resp => resp.json())
+            .then(leader => {
+                this.props.addLeader(leader)
+            })
+        } else {
+            // fetch to 1 arbitrarily because delete has to go to an id
+            fetch(`http://localhost:3000/follows/1`, fetchObj)
+            .then(resp => resp.json())
+            .then(data => {
+                this.props.removeLeader(data.leader_id)
+            })
+        }
+    }
+
+    renderFollowButton = () => {
+        if (this.props.leaders && this.props.leaders.find(leader => leader.id === this.state.user.id)) {
+            return (
+                 <Button onClick={() => this.followFetch('DELETE')} animated>
+                    <Button.Content visible>Following</Button.Content>
+                    <Button.Content hidden>Unfollow</Button.Content>
+                </Button>
+            )
+        } else {
+            return (
+                <Button onClick={() => this.followFetch('POST')}>
+                    <Button.Content>Follow</Button.Content>
+                </Button>
+            )
+        }
+    }
+
     renderProfileCard = () => {
         return (
             <Grid.Column width={6} style={{marginLeft: '5em', marginTop: '2em'}}>
@@ -42,7 +89,7 @@ class Profile extends React.Component {
                         </Card.Meta>
                     </Card.Content>
                 </Card>
-                { this.props.user && this.props.user.id === this.state.user.id ? <Button onClick={this.handleEditProfile} content='Edit Profile' /> : null }
+                { this.props.user && this.props.user.id === this.state.user.id ? this.renderEditButton() : this.renderFollowButton() }
             </Grid.Column>
         )
     }
@@ -68,8 +115,16 @@ class Profile extends React.Component {
 
 const mapStateToProps = state => {
     return {
-        user: state.user
+        user: state.user,
+        leaders: state.leaders
     }
 }
 
-export default connect(mapStateToProps)(WithAuth(Profile))
+const mapDispatchToProps = dispatch => {
+    return {
+        addLeader: leader => dispatch(addLeader(leader)),
+        removeLeader: leader_id => dispatch(removeLeader(leader_id))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(WithAuth(Profile))
