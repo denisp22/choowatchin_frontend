5d9cf42d5fd9f974485abbc07045495b8a313bd8
denisp22 <denispetrov722@gmail.com>
2020-01-17 18:12:39 -0600

    Attempt to upload pic file

--- src/containers/EditProfile.js
+++ src/containers/EditProfile.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import WithAuth from '../components/WithAuth'
 import { connect } from 'react-redux'
-import { Grid, Form, Image, Button } from 'semantic-ui-react'
+import { Grid, Form, Image, Button, Input } from 'semantic-ui-react'
 import { setUser } from '../actions/index'
+import ImageUploader from 'react-images-upload';
 
 class EditProfile extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             fullName: '',
-            email: ''
+            email: '',
+            uploadToggle: false
         }
     }
     
@@ -40,6 +42,7 @@ class EditProfile extends React.Component {
     handleSubmit = () => {
         // fetch PUT for user
         // send state
+
         const reqBody = {
             full_name: this.state.fullName,
             email: this.state.email
@@ -68,27 +71,73 @@ class EditProfile extends React.Component {
         localStorage.removeItem('token')
         this.props.history.push('/login')
     }
+
+    onDrop = (picture) => {
+        const reqBody = {
+            avatar: picture
+        }
+
+        const reqObj = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(reqBody)
+        }
+        
+        fetch(`http://localhost:3000/users/${this.props.user.id}`, reqObj)
+        .then(resp => resp.json())
+        .then(avatar => console.log(avatar))
+    }
+
+    handleUpload = (event) => {
+        // this.setState({avatar: event.target.files[0]})
+        const formData = new FormData()
+        formData.append('avatar', event.target.files[0])
+        // data.append('data', 'data')
+        const reqObj = {
+            method: 'PUT',
+            body: formData
+        }
+        
+        fetch(`http://localhost:3000/users/${this.props.user.id}`, reqObj)
+        .then(resp => resp.json())
+        .then(avatar => console.log(avatar))
+    }
+
+    handleImageUpload = () => {
+        return (
+            <Input type='file' onChange={this.handleUpload}/>
+        )
+    }
+
+    renderEditColumn = () => {
+        return (
+            <Grid.Column style={{marginLeft: '4em'}}>
+                <h1 style={{textAlign: 'center', marginBottom: '5em'}}>Edit Profile</h1>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Field>
+                        <label>Full Name</label>
+                        <input onChange={this.changeName} value={this.state.fullName} placeholder={this.props.user ? this.props.user.full_name : null} />
+                        <label style={{marginTop: '1em'}}>Email</label>
+                        <input onChange={this.changeEmail} value={this.state.email} placeholder={this.props.user ? this.props.user.email : null} />
+                    </Form.Field>
+                    <Form.Button>Update Profile</Form.Button>
+                </Form>
+                <Button style={{marginTop: '1em', color: 'red'}} onClick={this.handleDelete} content='Delete User' />
+            </Grid.Column>
+        )
+    }
     
     render() {
         return (
             <Grid columns={3}>
                 <Grid.Column style={{textAlign: 'center', marginLeft: '5em', marginTop: '5em'}}>                   
                     <Image src={this.state.pic} style={{marginBottom: '2em'}}/>
-                    <Button content='Change Profile Picture' />
+                    <Button onClick={() => this.setState({uploadToggle: true})} content='Change Profile Picture' />
+                    {this.state.uploadToggle ? this.handleImageUpload() : null}
                 </Grid.Column>
-                <Grid.Column style={{marginLeft: '4em'}}>
-                    <h1 style={{textAlign: 'center', marginBottom: '5em'}}>Edit Profile</h1>
-                    <Form onSubmit={this.handleSubmit}>
-                        <Form.Field>
-                            <label>Full Name</label>
-                            <input onChange={this.changeName} value={this.state.fullName} placeholder={this.props.user ? this.props.user.full_name : null} />
-                            <label style={{marginTop: '1em'}}>Email</label>
-                            <input onChange={this.changeEmail} value={this.state.email} placeholder={this.props.user ? this.props.user.email : null} />
-                        </Form.Field>
-                        <Form.Button>Update Profile</Form.Button>
-                    </Form>
-                    <Button style={{marginTop: '1em', color: 'red'}} onClick={this.handleDelete} content='Delete User' />
-                </Grid.Column>
+                {this.renderEditColumn()}
             </Grid>
         )
     }
