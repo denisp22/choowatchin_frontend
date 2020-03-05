b2f977bfbb945618be5aafcb89cf082f89806ba3
denisp22 <denispetrov722@gmail.com>
2020-03-04 22:00:59 -0500

    Test react responsive package

--- src/App.js
+++ src/App.js
@@ -15,35 +15,43 @@ import Friends from './containers/Friends'
 import Profile from './containers/Profile'
 import EditProfile from './containers/EditProfile'
 import SearchPage from './containers/SearchPage'
+import MediaQuery from 'react-responsive'
 
 function App() {
   
   return (
-    <BrowserRouter>
-      <Grid>
-        <Grid.Row style={{ marginBottom: '4em'}}>
-          <Navbar />
-        </Grid.Row>
-        <Grid.Row >
-          <Switch>
-            <Route exact path='/' component={Login} />
-            <Route path='/login' component={Login} />>
-            <Route path='/signup' component={Signup} />
-            <Route path='/home' component={Dashboard} />
-            <Route exact path='/movies' component={InTheaters} />
-            <Route path='/movies/:id' component={MovieShow} />
-            <Route exact path='/series' component={OnAir} />
-            <Route path='/series/:id' component={SeriesShow} />
-            <Route path='/reviews/:medium/:id/new' component={CreateReview} />
-            <Route exact path='/friends' component={Friends} />
-            <Route exact path='/profile/:id' component={Profile} />
-            <Route path='/reviews/:id/edit' component={CreateReview} />
-            <Route path='/profile/:id/edit' component={EditProfile} />
-            <Route path='/search' component={SearchPage} />
-          </Switch>
-        </Grid.Row>
-      </Grid>
-    </BrowserRouter>
+    <React.Fragment>
+    <MediaQuery minDeviceWidth={1224} device={{ deviceWidth: 1600 }}>
+      <BrowserRouter>
+        <Grid>
+          <Grid.Row style={{ marginBottom: '4em'}}>
+            <Navbar />
+          </Grid.Row>
+          <Grid.Row >
+            <Switch>
+              <Route exact path='/' component={Login} />
+              <Route path='/login' component={Login} />>
+              <Route path='/signup' component={Signup} />
+              <Route path='/home' component={Dashboard} />
+              <Route exact path='/movies' component={InTheaters} />
+              <Route path='/movies/:id' component={MovieShow} />
+              <Route exact path='/series' component={OnAir} />
+              <Route path='/series/:id' component={SeriesShow} />
+              <Route path='/reviews/:medium/:id/new' component={CreateReview} />
+              <Route exact path='/friends' component={Friends} />
+              <Route exact path='/profile/:id' component={Profile} />
+              <Route path='/reviews/:id/edit' component={CreateReview} />
+              <Route path='/profile/:id/edit' component={EditProfile} />
+              <Route path='/search' component={SearchPage} />
+            </Switch>
+          </Grid.Row>
+        </Grid>
+      </BrowserRouter>
+      </MediaQuery>
+      <MediaQuery maxDeviceWidth={1224}>
+        <h1>Mobile friendly version on the way. Please us out on a computer in the meantime.</h1>
+      </MediaQuery>
+    </React.Fragment>
   );
 }
 
