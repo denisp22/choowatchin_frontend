f4bd7a5d466ed8c2ce0c515d024fe88cf5f48998
denisp22 <denispetrov722@gmail.com>
2020-07-09 15:14:03 -0400

    Create LoginResponsive

---
+++ src/containers/LoginResponsive.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import MediaQuery from 'react-responsive';
+import Login from './Login';
+// import FriendsMobile from './FriendsMobile'
+
+function FriendsResponsive() {
+    return (
+        <React.Fragment>
+            <MediaQuery minDeviceWidth={750}>
+                <Login />
+            </MediaQuery>
+            <MediaQuery maxDeviceWidth={749}>
+                {/* <FriendsMobile /> */}
+                <h1>Login Mobile</h1>
+            </MediaQuery>
+        </React.Fragment>
+    )
+}
+
+export default LoginResponsive;
